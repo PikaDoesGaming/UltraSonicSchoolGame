6a3c9c5f3157701f95d58a10403d4c1e213473a6
PikaDoesGamig <Pikachu1712Ultra@gmail.com>
2018-06-02 11:53:32 +0200

    Fixed Highscores.py Functions.py added

--- Highscores.py
+++ Highscores.py
@@ -2,10 +2,6 @@ import pickle
 import time
 
 
-Highscores = []
-Top_10 = []
-
-# Get today in DD/MM/YYYY
 def date():
     current_year = time.localtime().tm_year
     current_month = time.localtime().tm_mon
@@ -13,7 +9,12 @@ def date():
     return_string = str(current_day) + " / " + str(current_month) + " / " + str(current_year)
     return return_string
 
-# When a 11th score is added  -> delete the lowest one
+
+Highscores = []
+
+Top_10 = []
+
+
 def remove_lowest_score():
     if len(Highscores) > 10:
         scores = []
@@ -25,7 +26,7 @@ def remove_lowest_score():
     else:
         return Highscores
 
-# Add new highscore
+
 def new_highscore(name, highscore, date):
     locallist = []
     locallist.append(name)
@@ -35,33 +36,46 @@ def new_highscore(name, highscore, date):
     Top_10.append(locallist)
     return remove_lowest_score()
 
-# load top 10 highscores from file
-def load_highscores():
-    with open("highscore.txt", "rb") as highscorefile:
-        Top_10 = pickle.load(highscorefile)
-    return Top_10
 
-# write top 10 highscores to file
-def write_highscores(Top_10):
-    with open("highscore.txt", "wb") as highscorefile:
-        Top_10 = pickle.dump(Top_10, highscorefile)
-
-# Test data
 user_name = "Pika"
 user_score = 8700
 user_date = date()
 
 new_highscore(user_name, user_score, user_date)
 print(Top_10)
-new_highscore("Example Person", 8600, date())
+new_highscore("User1", 8600, date())
 print(Top_10)
-new_highscore("Example Person 2", 8500, date())
+new_highscore("User2", 8500, date())
 print(Top_10)
+new_highscore("User3", 8500, date())
+print(Top_10)
+new_highscore("User4", 8500, date())
+print(Top_10)
+new_highscore("User5", 8500, date())
+print(Top_10)
+new_highscore("User6", 8500, date())
+print(Top_10)
+new_highscore("User7", 8500, date())
+print(Top_10)
+new_highscore("User8", 8500, date())
+print(Top_10)
+new_highscore("User9", 8500, date())
+print(Top_10)
+new_highscore("User10", 8500, date())
+print(Top_10)
+new_highscore("User11", 8500, date())
+print(Top_10)
+
+
+def load_highscores():
+    with open("highscore.txt", "rb") as highscorefile:
+        Top_10 = pickle.load(highscorefile)
+    return Top_10
+
+
+def write_highscores(Top_10):
+    with open("highscore.txt", "wb") as highscorefile:
+        Top_10 = pickle.dump(Top_10, highscorefile)
+
 
 write_highscores(Top_10)
-
-
-
-
-
-
